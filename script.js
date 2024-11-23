a9aa62f7a10234c56375804843bccf614d3ae39a
vdesimone <173499012+vdesimone@users.noreply.github.com>
2024-11-23 16:23:10 -0500

    Remove `title` and `desc` tags from `checkSVG`, and remove excess space in HTML

--- script.js
+++ script.js
@@ -481,13 +481,18 @@ window.App = {
 
       // SVGs for task buttons
       const checkSVG = App.utils.createSVG(
-        "Mark Task as Complete",
-        "Button to check off your task as complete. The icon changes to indicate completion.",
+        undefined,
+        undefined,
         "M9.9997 15.1709L19.1921 5.97852L20.6063 7.39273L9.9997 17.9993L3.63574 11.6354L5.04996 10.2212L9.9997 15.1709Z",
         "0 0 24 24",
         "36",
         "36"
       );
+      const checkSVGTitle = checkSVG.querySelector("title");
+      const checkSVGDesc = checkSVG.querySelector("desc");
+      checkSVG.removeChild(checkSVGTitle);
+      checkSVG.removeChild(checkSVGDesc);
+
       const pencilSVG = App.utils.createSVG(
         "Edit Task",
         "Button to edit your current task.",
